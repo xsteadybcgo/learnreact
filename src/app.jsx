9e7891bb2e299b594425afd6dc92a8276a0f000e
xiangwen <xiangwen@xhqb.com>
2018-06-21 09:42:54 +0800

    webpack initial setting

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import css from './index.css';
+import scss from './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css'
+ReactDOM.render(
+  <div>
+    <i className="fa fa-address-book"></i>
+    <h1>Hello, world!</h1>
+  </div>,
+  document.getElementById('app')
+);
