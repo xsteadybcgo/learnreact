e8e8566c11c995cca21c9b08d83556d08e2e58c1
xiangwen <xiangwen@xhqb.com>
2018-06-22 14:29:52 +0800

    基础搭建与学习

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # learnreact
 react todo
+
+### 创建组建的生命周期
+- constructor
+- componentWillMount
+- render
+- componentDidMount
+
+### state更新组建的生命周期
+- shouldComponentUpdate
+- componentWillUpdate
+- render
+- componentDidUpdate
+
+### props更新组建的生命周期
+- componentWillReceiveProps
+- shouldComponentUpdate
+- componentWillUpdate
+- render
+- componentDidUpdate
+----------------------------------
+### React-Router4
+
+> route
++ Route props 包含(match location history)
++ Route render methods（render/component/children） | render  优于 component <https://reacttraining.com/react-router/web/api/Route/render-func>
+
--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,78 @@
-import css from './index.css';
-import scss from './index.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.min.css'
+import css from "./index.css";
+import scss from "./index.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import "font-awesome/css/font-awesome.min.css";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+class A extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {  
+        return (
+            <h1>Component A</h1>
+        );
+    }
+}
+class B extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {  
+        return (
+            <h1>Component b</h1>
+        );
+    }
+}
+class Tab extends React.Component{
+    constructor(){
+        super()
+    }
+    render() {
+        return (
+            <div>
+                <Link to="/a">A组件</Link>
+                <Link to="/a/123">A带参数组件</Link>                
+                <Link to="/b">B组件</Link>
+                <h1>{this.props.children}</h1>
+            </div>
+        )
+    }
+}
+class App extends React.Component{
+    constructor(){
+        super()
+    }
+    componentWillMount()  {
+
+    }
+    componentDidMount() {
+
+    }
+    componentWillReceiveProps() {
+
+    }
+    componentWillUpdate() {
+
+    }
+    componentWillReceiveProps() {}
+
+    render(){
+        return (
+            <div>
+                <Title>
+                    <span> children component !!! </span>
+                </Title>
+            </div> 
+        );
+    }
+}
 ReactDOM.render(
-  <div>
-    <i className="fa fa-address-book"></i>
-    <h1>Hello, world!</h1>
-  </div>,
-  document.getElementById('app')
+    <Router>
+        <Tab>
+            <Route path="/a" component={A} />
+            <Route path="/b" component={B} />   
+        </Tab>
+    </Router>,
+  document.getElementById("app")
 );
